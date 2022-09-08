3a3af87b7a07814a41d7b012e5c43276763dc3aa
Prateek Sharma <prateekrichu@gmail.com>
2022-09-07 22:40:33 -0400

    url change

--- src/main/java/com/tuck/matches/beans/Constants.java
+++ src/main/java/com/tuck/matches/beans/Constants.java
@@ -2,7 +2,7 @@ package com.tuck.matches.beans;
 
 public class Constants {
 	
-	public static final String ORIGIN_URL = "https://schedularfortuck.netlify.app";
+	public static final String ORIGIN_URL = "https://tccschedular.netlify.app";
 	public static final String EMAIL_ID = "tccschedular@gmail.com";
 	public static final String PASSWORD = "hlvmjyvxijqexssj";
 //	public static final String ORIGIN_URL = "http://localhost:3000";
