aa1d6297cb0b10ba250ea31bd3f4ef5a10c7b299
Prateek Sharma <prateekrichu@gmail.com>
2022-09-24 15:21:50 -0400

    app pass

--- src/main/java/com/tuck/matches/beans/Constants.java
+++ src/main/java/com/tuck/matches/beans/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	public static final String ORIGIN_URL = "https://tccschedular.netlify.app";
 	public static final String EMAIL_ID = "tccschedular@gmail.com";
-	public static final String PASSWORD = "hlvmjyvxijqexssj";
+	public static final String PASSWORD = "xoqwwupflunrucaj";
 //	public static final String ORIGIN_URL = "http://localhost:3000";
 
 
