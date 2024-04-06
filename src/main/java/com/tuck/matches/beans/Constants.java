08faae31ed217a2af7e56f2b282c4c6631776510
Prateek Sharma <109249875+prateeksharma1809@users.noreply.github.com>
2024-04-06 14:35:56 -0400

    Update Constants.java

--- src/main/java/com/tuck/matches/beans/Constants.java
+++ src/main/java/com/tuck/matches/beans/Constants.java
@@ -2,7 +2,7 @@ package com.tuck.matches.beans;
 
 public class Constants {
 
-	public static final String EMAIL_ID = "gmailidr@gmail.com";
+	public static final String EMAIL_ID = "yourgmailid@gmail.com";
 	public static final String PASSWORD = "gmail_app_password";
 	public static final String ORIGIN_URL = "http://localhost:3000";
 
