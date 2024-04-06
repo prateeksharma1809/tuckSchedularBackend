8fe18d06c467c3af06a18c5e66a02c72e23c6e83
Prateek Sharma <109249875+prateeksharma1809@users.noreply.github.com>
2024-04-06 14:35:40 -0400

    Update Constants.java

--- src/main/java/com/tuck/matches/beans/Constants.java
+++ src/main/java/com/tuck/matches/beans/Constants.java
@@ -1,10 +1,9 @@
 package com.tuck.matches.beans;
 
 public class Constants {
-	
-//	public static final String ORIGIN_URL = "https://tccschedular.netlify.app";
-	public static final String EMAIL_ID = "tccschedular@gmail.com";
-	public static final String PASSWORD = "xoqwwupflunrucaj";
+
+	public static final String EMAIL_ID = "gmailidr@gmail.com";
+	public static final String PASSWORD = "gmail_app_password";
 	public static final String ORIGIN_URL = "http://localhost:3000";
 
 
